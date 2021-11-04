956260ec8efc5f183a3738df3f6541253873f968
sh1rokovs <hilallod@yandex.ru>
2021-11-04 18:21:14 +0300

    callback

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+	validates :price, numericality: { greater_than: 0 }
+	validates :name, :description, presence: true
+
+	after_initialize { p 'initialize'} # Item.new Item.first
+	after_save       { p 'save'}	# Item.create Item.save
+	after_create     { p 'create'} # Item.create
+	after_update     { p 'update'} # Item.update
+	after_destroy    { p 'destroy'} # Item.last.destroy
 end
